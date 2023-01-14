acc2ab3ee2d5ec669c6726152a5cf90f4c71fb51
smoukeuur <enzotang@live.fr>
2023-01-14 20:05:52 +0100

    Finalisation Dropdown

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,7 +94,7 @@ window.onload = function () {
         labels: labels,
         datasets: [
           {
-            label: "Vols par semaine",
+            label: "Vols par mois",
             backgroundColor: [
               "#3e95cd",
               "#8e5ea2",
@@ -122,7 +122,7 @@ window.onload = function () {
       },
     });
   }
-  
+
   var map = L.map("graphique3").setView([0, 0], 2);
 
   L.tileLayer(
@@ -133,7 +133,7 @@ window.onload = function () {
     }
   ).addTo(map);
 
-  var data = ".assets/airports.csv";
+  var data = "./assets/airports.csv";
   var lines = data.split("\n");
   var markers = [];
   for (var i = 1; i < lines.length; i++) {
@@ -170,54 +170,30 @@ select2 = $('#compagnie');
 specsLi = $('#specs');
 
 $.ajax({
-    url: '../assets/json/wikiplane.json',
-    dataType:'JSON',
-    success:function(data){
-      // console.log(data.jsonWiki[0].Boeing777.constructeur);
-        select.html('');
-        $.each(data.jsonWiki, function (val) {
-          console.log(data.jsonWiki[val].Boeing777.constructeur);
-            select.append('<option id="' + data.jsonWiki[val].Boeing777.idAvion + '">' + data.jsonWiki[val].Boeing777.constructeur + " " + data.jsonWiki[val].Boeing777.modele + '</option>');
-        })
-    },
-    error:function(){
-        $select.html('<option id="-1">Aucune compagnie disponible</option>');
-    },
-
-});
-
-$.ajax({
   url: '../assets/json/wikiplane.json',
-  dataType:'JSON',
-success:function(data2){
-  console.log(data2.jsonWiki[0].compagnie.nomCompagnie);
-    select2.html('');
-    $.each(data2.jsonWiki, function (val2) {
-      // console.log(data2.jsonWiki[val2].compagnie.nomCompagnie);
-        select2.append('<option id="' + data2.jsonWiki[val2].compagnie.idCompagnie + '">' + data2.jsonWiki[val2].compagnie.nomCompagnie + '</option>');
+  dataType: 'JSON',
+  success: function (data) {
+    // console.log(data.jsonWiki[0].Boeing777.constructeur);
+    $.each(data.jsonWiki, function (val2) {
+      // console.log(data.jsonWiki[val2].compagnie.nomCompagnie);
+      select2.append('<option data-nom = "' + data.jsonWiki[val2].compagnie.nomCompagnie + '" value="' + data.jsonWiki[val2].constructeur + " " + data.jsonWiki[val2].modele + ',' + data.jsonWiki[val2].imageSrc + '">' + data.jsonWiki[val2].compagnie.nomCompagnie + '</option>');
     })
-},
-error:function(){
+    $("select").on('change', function (e) {
+      // console.log($(this).val());
+      // $("#modeleAvion option").val($(this).val());
+      let tab = $(this).val();
+      tab = tab.split(",");
+      console.log(tab);
+      $("#modeleAvion option").html(tab[0]);
+      $(".imageModele").attr('src', tab[1]);
+      $("#compagnie").click(function(){
+        $(".imageModele").animate({opacity: '1',});
+      });
+    })
+  },
+  error: function () {
+    $select.html('<option id="-1">Aucune compagnie disponible</option>');
     $select2.html('<option id="-1">Aucune compagnie disponible</option>');
-}
-});
+  },
 
-$.ajax({
-  url: '../assets/json/wikiplane.json',
-  dataType:'JSON',
-success:function(data3){
-  // console.log(data3.jsonWiki[0].compagnie.nomCompagnie);
-    specsLi.html('');
-    $.each(data3.jsonWiki, function (val3) {
-      // console.log(data3.jsonWiki[val3].compagnie.nomCompagnie);
-        specsLi.append('<li id="' + data3.jsonWiki[val3].Boeing777.idAvion + '">' + data3.jsonWiki[val3].Boeing777.specifications.longueur + '</li>');
-        specsLi.append('<li id="' + data3.jsonWiki[val3].Boeing777.idAvion + '">' + data3.jsonWiki[val3].Boeing777.specifications.envergure + '</li>');
-        specsLi.append('<li id="' + data3.jsonWiki[val3].Boeing777.idAvion + '">' + data3.jsonWiki[val3].Boeing777.specifications.nbSieges + '</li>');
-        specsLi.append('<li id="' + data3.jsonWiki[val3].Boeing777.idAvion + '">' + data3.jsonWiki[val3].Boeing777.specifications.vitesse + '</li>');
-        specsLi.append('<li id="' + data3.jsonWiki[val3].Boeing777.idAvion + '">' + data3.jsonWiki[val3].Boeing777.specifications.typeMoteur + '</li>');
-    })
-},
-error:function(){
-    $specsLi.html('<li id="-1">Aucune compagnie disponible</li>');
-}
 });
